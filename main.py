e599897b679512296acce4aef9aaaf926d15f6dc
karterr1 <karterr.r@yandex.ru>
2026-09-26 01:57:55 +0500

    basic

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask
-app = Flask(name)
+from flask import Flask, render_template
+app = Flask(__name__)
+
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def index():
+  return render_template('base.html')
 
-if name == 'main':
-  app.run(debug=True)
+@app.route('/register')
+def register():
+  return render_template('register.html')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
